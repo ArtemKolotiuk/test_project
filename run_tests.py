b5fbf78adce1f1df53e88b245462a4e6d613d5b7
Колотюк Артем Вікторович <Artem.Kolotyuk@ENT.UKRGAS.COM.UA>
2021-12-22 18:44:38 +0200

    init_commit

---
+++ run_tests.py
@@ -0,0 +1,8 @@
+from api_tests import test_create_user, test_create_user_invalid_data, test_get_users, test_update_user
+
+
+if __name__ == '__main__':
+	#test_create_user()
+	#test_create_user_invalid_data()
+	#test_get_users()
+	test_update_user()
