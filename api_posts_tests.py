07d4be3637c22047662604f4649cb30e3dc68a0d
Колотюк Артем Вікторович <Artem.Kolotyuk@ENT.UKRGAS.COM.UA>
2022-01-12 17:02:59 +0200

    Освіжив тести на пости.

--- api_posts_tests.py
+++ api_posts_tests.py
@@ -1,108 +1,24 @@
-import requests
 import json
 import random
+
 import pytest
-from settings import status_ok, invalid_create_user_payload, user_created_invalid
-
-host = 'http://localhost'
-port = '5000'
-root_url = f"{host}:{port}"
-created = 201
-updated = 201
-success = 200
-bad_request = 400
-headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
-post_create_payload = {'title': 'a', 'text': 'b', 'author_id': 1}
-url_posts = f"{root_url}/posts"
-url_users = f"{root_url}/users"
-post_create_invalid_payload = {'text': 'b', 'author_id': 1}
-post_create_payload = {'title': 'a', 'text': 'b', 'author_id': 1}
-url_posts_id = f"{root_url}/posts/{1}"
+import requests
+from settings import root_url, created, updated, success, bad_request, headers, url_posts, post_create_invalid_payload, \
+    post_create_payload, url_posts_id
 
 
+# Create post
 def test_create_post():
-    try:
-        res = requests.post(url_posts, data=json.dumps(post_create_payload), headers=headers)
-        status = res.status_code
-        if status == created:
-            try:
-                body = res.json()
-                if body.get("title") != post_create_payload.get("title"):
-                    print(f"Wrong data in {body.get('title')}. Status code: {status}")
-                elif body.get("text") != post_create_payload.get("text"):
-                    print(f"Wrong data in {body.get('text')}. Status code: {status}")
-                elif body.get("author_id") != post_create_payload.get("author_id"):
-                    print(f"Wrong data-type in author_id: {body.get('author_id')} given. Status code: {status}")
-                else:
-                    print(f"Post created successfully!. Created data {body} Status code: {status}")
-                return True
-            except Exception:
-                raise Exception(f"Exception with jsonifying content: {res.content}")
-        else:
-            print(f"Post creation has failed with wrong response status code: {status}")
-    except Exception as e:
-        raise Exception(f"Request to {url_posts} failed with exception: {e}")
+    res = requests.post(url_posts, data=json.dumps(post_create_payload), headers=headers)
+    status = res.status_code
+    body = res.json()
+    assert status == created
+    assert body.get("title") == post_create_payload.get("title")
+    assert body.get("text") == post_create_payload.get("text")
+    assert body.get("author_id") == post_create_payload.get("author_id")
+           
 
-
-def test_get_users():
-    try:
-        res = requests.get(url_users)
-        status = res.status_code
-        if status == status_ok:
-            try:
-                body = res.json()
-                if type(body) == list:
-                    print(f"Users extracted successfully. Status-code: {status}")
-            except Exception as e:
-                raise Exception(f"Failed with  {res.content}")
-    except Exception as e:
-        raise Exception(f"Request to {url_users} failed with exception: {e}")
-
-
-def test_update_user():
-    res = requests.get(url_users)
-
-    users = res.json()
-    if users:
-        random_user = random.choice(users)
-        current_username = random_user.get("username")
-        random_user_id = random_user.get("id")
-        random_user_url = f"{url_users}/{random_user_id}"
-
-        updated_user_payload = random_user.copy()
-
-        updated_username = ''.join(random.sample(current_username, len(current_username)))
-        updated_user_payload["username"] = updated_username
-
-        res = requests.put(random_user_url, data=json.dumps(updated_user_payload), headers=headers)
-        updated_user = res.json()
-        if updated_user != random_user:
-            print(f"User {random_user} updated successfully to {updated_user}.")
-        else:
-            print(f"User updating failed, {updated_user} equals {random_user}.")
-    else:
-        print(f"Response body is empty, details: {users}")
-
-
-def test_create_user_invalid_data():
-    try:
-        res = requests.post(url_users, data=json.dumps(invalid_create_user_payload), headers=headers)
-        status = res.status_code
-        if status == user_created_invalid:
-            try:
-                body = res.json()
-                message = body.get("error_message")
-                if message:
-                    print(
-                        f"User wasn't created, cause invalid input data with status-code: {status} \n and error_message: {message}")
-            except Exception as e:
-                raise Exception(f"Failed with exception {e}")
-        else:
-            print(f"ERROR: User created with invalid data. With status-code {status}")
-    except Exception as e:
-        raise Exception(f"Request to {url_users} failed with exception: {e}")
-
-
+ # GET not existing posts
 def generate_not_exists_post_id(posts_ids):
     random_id = posts_ids[0]
     while random_id in posts_ids:
@@ -149,7 +65,6 @@ def test_get_post_check_by_body():
     """GET post/id"""
     resp = requests.get(url_posts)
     posts = resp.json()
-    posts = []
     if posts:
         post_id = posts[0].get("id")
         post_url = (f"{url_posts}/{post_id}")
@@ -188,17 +103,7 @@ def test_update_post_with_invalid_data():
     if posts:
         post_id = posts[0].get("id")
         post_url = f"{url_posts}/{post_id}"
-        res = requests.get(post_url)
-        post = res.json()
-        updated_post = post.copy()
-
-        current_title = post.get("title")
-        updated_title = ''.join(random.sample(current_title, len(current_title)))
-        updated_post["title"] = updated_title
-        print(updated_post)
-        del updated_post["author_id"]
-        print(updated_post)
-        update_post_resp = requests.put(post_url, data=json.dumps(updated_post), headers=headers)
+        update_post_resp = requests.put(post_url, headers=headers)
         assert update_post_resp.status_code == bad_request
 
 def test_update_post_with_invalid_id():
--- settings.py
+++ settings.py
@@ -8,3 +8,15 @@ headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
 user_create_payload = {'username': 'new_user', 'email': 'test@mail.com', 'password': '123'}
 url = f"{root_url}/users"
 invalid_create_user_payload = {'email': 'invalid_test@mail.com', 'password': '1234'}
+
+
+created = 201
+updated = 201
+success = 200
+bad_request = 400
+post_create_payload = {'title': 'a', 'text': 'b', 'author_id': 1}
+url_posts = f"{root_url}/posts"
+url_users = f"{root_url}/users"
+post_create_invalid_payload = {'text': 'b', 'author_id': 1}
+post_create_payload = {'title': 'a', 'text': 'b', 'author_id': 1}
+url_posts_id = f"{root_url}/posts/{1}"
